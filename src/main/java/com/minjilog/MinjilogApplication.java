bed2930c468f12b74ec93dc3104a72d056dc521e
jungminji <wjdalswl0215@gmail.com>
2024-07-31 03:59:38 +0900

    first commit

---
+++ src/main/java/com/minjilog/MinjilogApplication.java
@@ -0,0 +1,13 @@
+package com.minjilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjilogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinjilogApplication.class, args);
+    }
+
+}
